ca077894e8ba2446ff818de868a4ab57d67cc2f8
lucasprates74 <79284676+lucasprates74@users.noreply.github.com>
2022-09-12 15:20:45 -0400

    Added method prateslu to IntroLab and relevant test in IntroLabTest

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 100)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -30,4 +30,10 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+    @Test(timeout = 100)
+    public void testPrateslu() {
+        String expected = "Hey! My name is Lucas! I like cats more than dogs! Java is fun";
+        String actual = IntroLab.prateslu();
+        assertEquals(expected, actual);
+    }
 }
